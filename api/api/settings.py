b0492300786ef71b61d536a32e99e1d76fdce32e
afarut <65365788+afarut@users.noreply.github.com>
2023-07-19 02:20:56 +0300

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-iwj4u3i25_tqlqj5)70oe5*g#4y-w941#fs)l%vb2ke(vp3cv7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dubai.elirm.ru"]
 
 
 # Application definition
