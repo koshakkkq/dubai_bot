32feece35a89902a00274abd8a531c5ea3a7d8e5
Koshak <kokolen77@gmail.com>
2023-07-29 17:15:24 +0300

    recivers fix

--- api/core/apps.py
+++ api/core/apps.py
@@ -5,3 +5,6 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
     verbose_name = 'shop'
+
+    def ready(self):
+        from . import recivers
--- api/core/models.py
+++ api/core/models.py
@@ -5,7 +5,6 @@ from django.db.models import F
 from django.db.models.signals import pre_delete, pre_save, post_save
 
 from .constants import VERBOSE_ORDER_TYPE, VERBOSE_RAITING_TYPE, OrderStatus
-from django.dispatch.dispatcher import receiver
 class Courier(models.Model):
 	name = models.CharField(max_length=100)
 	phone = models.CharField(max_length=100)
@@ -40,7 +39,6 @@ class TelegramUser(models.Model):
 
 	def __str__(self):
 		return f"Tg id: {self.telegram_id}"
-
 
 	class Meta:
 		verbose_name = "Telegram user"
@@ -220,52 +218,6 @@ class MessageToDelete(models.Model):
 	tg_id = models.BigIntegerField()
 	msg_id = models.BigIntegerField(default=None, null=True)
 
-@receiver(pre_delete, sender=TelegramUser)
-def wrapper(sender, instance: TelegramUser, *args, **kwargs):
-	tg_id = instance.telegram_id
-	CourierRegistrationCode.objects.filter(user=tg_id).delete()
-	ShopRegistrationCode.objects.filter(user=tg_id).delete()
-
-@receiver(pre_delete, sender=ShopMember)
-def wrapper(sender, instance: ShopMember, *args, **kwargs):
-	tg_id = instance.user.telegram_id
-	ShopRegistrationCode.objects.filter(user=tg_id).delete()
-
-@receiver(post_save, sender=Shop)
-def wrapper(sender, instance: Shop, raw, update_fields, *args, **kwargs):
-	if kwargs['created'] is True:
-		ShopNotification.objects.create(shop=instance)
-
-
-@receiver(post_save, sender=TelegramUser)
-def wrapper(sender, instance, raw, update_fields, *args, **kwargs):
-	if kwargs['created'] is True:
-		UserNotification.objects.create(user=instance)
-
-@receiver(post_save, sender=OrderOffer)
-def wrapper(sender, instance: OrderOffer, raw, update_fields, *args, **kwargs):
-	user = instance.order.customer
-	if raw == False and instance.order.offer is None:
-		UserNotification.objects.filter(user=user).update(new_offers= F('new_offers') + 1)
-
-
-@receiver(pre_save, sender=Order)
-def wrapper(sender, instance: Order, raw, update_fields, *args, **kwargs):
-	try:
-		instance_in_db = Order.objects.get(id=instance.pk)
-		if instance_in_db.status == OrderStatus.PENDING and instance.status == OrderStatus.ACTIVE:
-			if instance.offer is not None:
-				ShopNotification.objects.update_or_create(
-					shop=instance.offer.shop,
-					defaults={
-						'new_active_orders': F('new_active_orders') + 1
-					}
-				)
-	except Order.DoesNotExist:
-		model = instance.model
-		shops_with_order_model = Shop.objects.filter(available_models=model)
-		notifications = ShopNotification.objects.filter(shop__in=shops_with_order_model)
-		notifications.update(new_available_orders=F('new_available_orders') + 1)
 
 
 
