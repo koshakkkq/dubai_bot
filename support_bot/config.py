588b33665248bf3b51c8f63793388a8b605f4c9e
Koshak <kokolen77@gmail.com>
2023-09-10 00:04:30 +0300

    Revert "all bot"
    This reverts commit ab2e4e26687a57a7a6f6a913a26957df02d21014.

--- support_bot/config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from decouple import config
-
-
-BASE_DIR = Path(__file__).resolve().parent
-API_TOKEN = config('BOT_TOKEN', default="", cast=str)
-GROUP_ID = config('GROUP_ID', default="", cast=int)
